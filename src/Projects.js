514595e44ac4184f370afbe67d8d6a763319e5fd
Michal Frystacky <Michal.Frystacky@alaska.gov>
2017-08-29 12:54:00 -0800

    Fix: Corrected unused lib lint warning

--- src/Projects.js
+++ src/Projects.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Card from 'react-md/lib/Cards/Card';
 import CardTitle from 'react-md/lib/Cards/CardTitle';
 import CardText from 'react-md/lib/Cards/CardText';
-import ProjectTile from './ProjectTile';
+
 
 const TournamentTracker = {
   projectTitle: 'Tournament Tracker',
