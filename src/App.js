a8e42462f7b3776987d5eae9d674eb9fa2505c3c
Michal Frystacky <Michal.Frystacky@gmail.com>
2017-08-29 22:09:50 -0800

    Fix: Removed custom-react-scripts to fix a build bug

--- src/App.js
+++ src/App.js
@@ -1,38 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import Projects from './Projects';
+import './App.css';
 import Profile from './Profile';
-// components
-import Emoji from './Emoji';
-
-// styles
-import './App.scss';
-import './style/scss/main.scss';
-
-// modules
-import cssStyles from './First.module.css';
-import sassStyles from './Second.module.scss';
-
-// eslint-disable-next-line
-const Test = () =>
-  (<div className="App">
-    <div className={cssStyles.header}>
-      <img src={logo} className="App-logo" alt="logo" />
-      <h2 className="App-title">
-        <Emoji label="danger" emoji="☢" />
-        <span> custom-react-scripts </span>
-        <Emoji label="danger" emoji="☢" />
-      </h2>
-      <div className="App-subtitle">allow custom config for create-react-app without ejecting</div>
-    </div>
-
-    <div className={sassStyles.command}>
-      <code>create-react-app my-app --scripts-version custom-react-scripts</code>
-    </div>
-  </div>);
+import Projects from './Projects';
 
 const App = () =>
   (<main>
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to React</h2>
+      </div>
+    </div>
     <header className="md-grid" />
     <Profile />
     <Projects />
