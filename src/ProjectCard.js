a5513493d6a6818e6c1e8c12c25808c8079e4ca0
Michal Frystacky <Michal.Frystacky@alaska.gov>
2017-08-31 18:22:46 -0800

    Feat: Converted to using Paper class and new style

--- src/ProjectCard.js
+++ src/ProjectCard.js
@@ -1,15 +1,15 @@
 import React from "react";
-import Card from "react-md/lib/Cards/Card";
+import Paper from "react-md/lib/Papers";
 import CardTitle from "react-md/lib/Cards/CardTitle";
 import CardText from "react-md/lib/Cards/CardText";
 
 const ProjectCard = ({ title, summary, tags }) =>
-  <div className="md-cell--6">
-    <Card>
+  <div className="md-cell md-cell--6">
+    <Paper zDepth={0} raiseOnHover={true}>
       <CardTitle title={title} />
       <CardText>
         {summary}
       </CardText>
-    </Card>
+    </Paper>
   </div>;
 export default ProjectCard;
--- src/Projects.js
+++ src/Projects.js
@@ -1,29 +1,37 @@
 import React from "react";
-import Card from "react-md/lib/Cards/Card";
-import CardTitle from "react-md/lib/Cards/CardTitle";
-import CardText from "react-md/lib/Cards/CardText";
+import ProjectCard from "./ProjectCard";
 
-const TournamentTracker = {
-  projectTitle: "Tournament Tracker",
-  summary:
-    "Multi-tournament swiss-pairing tracker build in Python3.0 and PostgreSQL",
-  imgLink: "https://via.placeholder.com/550x300"
-};
+const TournamentTracker = [
+  {
+    projectTitle: "Tournament Tracker",
+    summary:
+      "Multi-tournament swiss-pairing tracker build in Python3.0 and PostgreSQL",
+    imgLink: "https://via.placeholder.com/550x300"
+  },
+  {
+    projectTitle: "Movie Site",
+    summary: "Server side rendered Python 3 movie listing site"
+  }
+];
 
 const Projects = () =>
-  <div className="md-grid">
-    <div className="md-text-container">
-      <div className="md-cell md-cell--12">
-        <h2 className="md-title">Portfolio</h2>
+  <div>
+    <div className="md-grid">
+      <div className="md-text-container">
+        <div className="md-cell md-cell--12">
+          <h2 className="md-title">Portfolio</h2>
+        </div>
       </div>
-      <div className="md-cell--6">
-        <Card>
-          <CardTitle title={TournamentTracker.projectTitle} />
-          <CardText>
-            {TournamentTracker.summary}
-          </CardText>
-        </Card>
-      </div>
+    </div>
+    <div className="md-grid">
+      <ProjectCard
+        title={TournamentTracker[0].projectTitle}
+        summary={TournamentTracker[0].summary}
+      />
+      <ProjectCard
+        title={TournamentTracker[1].projectTitle}
+        summary={TournamentTracker[1].summary}
+      />
     </div>
   </div>;
 export default Projects;
